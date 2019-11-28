5c2895f0f354c3ee39e675e533a69cebb39dae03
MR.Phone <snowcat018@gmail.com>
2019-11-28 14:43:17 +0630

    6.2 Added About Us and Contact Us Page and modify scrollviews

--- app/src/main/java/com/example/sakuraboutique/UI/AboutUs.java
+++ app/src/main/java/com/example/sakuraboutique/UI/AboutUs.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.graphics.PorterDuff;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.MenuItem;
@@ -15,13 +16,14 @@ import android.widget.TextView;
 import com.example.sakuraboutique.R;
 
 public class AboutUs extends AppCompatActivity {
-TextView tvContactUs;
+TextView tvContactUs,tvFindUsonFacebook;
 Toolbar tbToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
         tvContactUs=findViewById(R.id.tvContactUs);
+        tvFindUsonFacebook=findViewById(R.id.tvFindUsonFacebook);
 
         tbToolbar=findViewById(R.id.tbToolbar);
 setSupportActionBar(tbToolbar);
@@ -33,6 +35,16 @@ getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             @Override
             public void onClick(View v) {
                 Intent i=new Intent(AboutUs.this,ContactUs.class);
+                startActivity(i);
+            }
+        });
+        tvFindUsonFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String url = "https://www.facebook.com/Sakura-Boutique-433408814113660/";
+                Intent i = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse(url));
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/sakuraboutique/UI/ContactUs.java
+++ app/src/main/java/com/example/sakuraboutique/UI/ContactUs.java
@@ -4,24 +4,41 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.graphics.PorterDuff;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.sakuraboutique.R;
 
 public class ContactUs extends AppCompatActivity {
 Toolbar toolbar;
+TextView tvLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
         toolbar=findViewById(R.id.tbToolbar);
+        tvLocation=findViewById(R.id.tvLocation);
+
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(Html.fromHtml("<font color='#FFFFFF'>" + "Contact Us" + " </font>"));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
+tvLocation.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        String map = "http://maps.google.co.in/maps?q=" + "4th floor, 53 21st St, Yangon, Myanmar (Burma)";
+        Intent i = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse(map));
+        startActivity(i);    }
+});
+
+
 
     }
 
